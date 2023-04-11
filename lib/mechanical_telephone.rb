d8c67941263eef0f0e5211bed5ce441e248018df
juwpan <juwpan@gmail.com>
2023-04-11 22:43:22 +0400

    added MechanicalTelephone class

---
+++ lib/mechanical_telephone.rb
@@ -0,0 +1,56 @@
+# Создание абстрактного класса Telephone
+class MechanicalTelephone < Telephone
+  # Инкапсуляция (можно получить данные свойств но не изменять их)
+  attr_reader :type, :color
+
+  # Конструктор наследует свойства от Telephone и добавляет два новых
+  # color, type
+  def initialize(*args, type, color)
+    super(*args)
+    @type = type
+    @color = color
+  end
+
+  # Полиморфизм (от класса Telephone реализация в ином виде)
+  def call(array_answers)
+    answer = array_answers.sample
+    stop_call_rus(answer)
+  end
+  
+  # Полиорфизм от класса to_s выводит данные о телефоне  c наследованием от Telephone
+  def to_s
+    super
+    puts <<~TELEPHONE
+    color: #{@color}
+    type: #{@type}
+    TELEPHONE
+  end
+
+  # Инкапсуляция (скрытая реализация от внешних воздействий)
+  private
+
+  # Возвращает ответ если звонок удался
+  def response
+    'Да.Слушаю?'
+  end
+
+  # Возвращает ответ если зволнок не удался
+  def mistake_call
+    'Абонент занят или в не зоны доступа...перезвоните позже'
+  end
+
+  # В зависимоти от ответа будет выводить вариант ответа или звонок удался или нет
+  def stop_call_rus(answer)
+    if answer == 'Yes'
+      rand(@@beep).times do
+        puts 'Гудок!'
+      end
+        response
+    else
+      @@beep.times do
+        puts 'Гудок!'
+      end
+        mistake_call
+    end
+  end
+end
